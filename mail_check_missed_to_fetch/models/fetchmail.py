00e672d463735f2e04fcf4bbfdbe02bbe30ae063
Marc Wimmer <marc@itewimmer.de>
2022-02-07 13:39:25 +0100

    Update fetchmail.py

--- mail_check_missed_to_fetch/models/fetchmail.py
+++ mail_check_missed_to_fetch/models/fetchmail.py
@@ -6,7 +6,7 @@ class MissingEntry(models.Model):
     _name = 'mail.missed.fetch'
 
     subject = fields.Char("Subject")
-    date = fields.Date("Sent")
+    datetime = fields.Date("Sent")
     missing_ok = fields.Boolean("Missing OK")
     mail_message_id = fields.Char("Message-ID", index=True)
 
