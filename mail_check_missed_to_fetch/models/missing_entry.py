85ed81cf975931dd0c789dfd750a9af677d56987
ipe <ipe>
2022-02-07 23:35:08 +0100

    [13.0]

--- mail_check_missed_to_fetch/models/missing_entry.py
+++ mail_check_missed_to_fetch/models/missing_entry.py
@@ -16,21 +16,21 @@ class MissingEntry(models.Model):
     def make_entry(self, fetchmail_server, parsed_message):
         message_id = parsed_message['message-id'].strip()
         self.env.cr.execute("select count(*) from mail_message where message_id=%s", (message_id,))
-        missing = self.env.cr.fetchone()[0]
-        existing = self.sudo().search([('mail_message_id', '=', message_id)])
+        existing_mail_message = self.env.cr.fetchone()[0]
+        existing_missed_record = self.sudo().search([('mail_message_id', '=', message_id)])
         try:
             date = datetime.strptime(parsed_message['Date'], "%a, %d %b %Y %H:%M:%S %z")
         except:
             date = False
-        if missing and not existing:
+        if not existing_mail_message and not existing_missed_record:
             self.create({
                 'mail_message_id': message_id,
                 'date': date,
                 'subject': parsed_message['Subject'],
                 'fetchmail_server_id': fetchmail_server.id,
             })
-        elif not missing and existing:
-            existing.unlink()
+        elif noexisting_mail_messaget and existing_missed_record:
+            existing_missed_record.unlink()
 
     _sql_constraints = [
         ('mail_message_id_unique', "unique(mail_message_id)", _("Only one unique entry allowed.")),
